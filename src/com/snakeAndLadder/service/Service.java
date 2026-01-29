0284fdd146041cff1faaa61f023a4fd97f3dec2e
arsath ali <arsatharsha17@gmail.com>
2026-01-29 16:37:18 +0530

    [Arsath] feature(UC3): implement player option check for No Play, Ladder, or Snake

--- src/com/snakeAndLadder/service/Service.java
+++ src/com/snakeAndLadder/service/Service.java
@@ -2,18 +2,40 @@ package com.snakeAndLadder.service;
 
 import java.util.Random;
 
+import com.snakeAndLadder.constants.GameConstants;
 import com.snakeAndLadder.model.Player;
 import com.snakeAndLadder.util.DiceUtil;
 
 public class Service {
-    
+
+     private final Random random = new Random();
+     
     public void playTurn(Player player){
         
-        int  diceValue = DiceUtil.rollDice();
-
+       int diceValue = DiceUtil.rollDice();
         System.out.println("Dice rolled: " + diceValue);
 
-        player.setPosition(player.getPosition() + diceValue);
-        System.out.println("Player new position: "+player.getPosition());
+        int option = random.nextInt(3); // 0–2
+        String optionName = "";
+
+        switch (option) {
+            case GameConstants.NO_PLAY:
+                optionName = "No Play";
+                // position unchanged
+                break;
+
+            case GameConstants.LADDER:
+                optionName = "Ladder";
+                player.setPosition(player.getPosition() + diceValue);
+                break;
+
+            case GameConstants.SNAKE:
+                optionName = "Snake";
+                player.setPosition(player.getPosition() - diceValue);
+                break;
+        }
+
+        System.out.println("Option: " + optionName);
+        System.out.println("Player new position: " + player.getPosition());
     }
 }
